49a636f33f9dde0383d8cec6b120fc0ca04ff959
Victor <88071929+victormrtns@users.noreply.github.com>
2024-04-11 18:12:58 -0300

    feat - using routes to get and post user inside an array, app stateful

--- src/server.js
+++ src/server.js
@@ -4,7 +4,24 @@
 import http from 'node:http'
 
 //Req from my request object
+//Stateful - Stateless (Data in memory, Data inside databases,text,etc )
+const users = []
 const server = http.createServer((req,res)=>{
-  return res.end('Hello woralad')
+  const {method,url} = req
+  if(method === 'GET' && url==='/users'){
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if(method === 'POST' && url==='/users'){
+    users.push({
+      id:1,
+      name:'John Doe',
+      email:'john@example.com'
+    })
+    return res.writeHead(201).end()
+  }
+  return res.writeHead(404).end('Process not found')
 })
 server.listen(3333)
